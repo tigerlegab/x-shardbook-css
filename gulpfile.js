7a5d7ae95c87f32d7138e6d972bbe14b8befa7bf
gabriel <gabrielboltron@gmail.com>
2021-06-01 18:58:29 +0800

    ~v0.1.2 - update antd to 4.16.1

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,17 @@ const cssnano = require('cssnano');
 const NpmImportPlugin = require('less-plugin-npm-import');
 
 gulp.task('build:less', function () {
-    const plugins = [
-        autoprefixer(),
-        cssnano()
-    ];
-    return gulp
-        .src('styles/*.less')
-        .pipe(debug({ title: 'Less files:' }))
-        .pipe(gulpless({
-            javascriptEnabled: true,
-            plugins: [new NpmImportPlugin({ prefix: '~' })]
-        }))
-        .pipe(postcss(plugins))
-        .pipe(gulp.dest('dist'))
+   const plugins = [
+      autoprefixer(),
+      cssnano()
+   ];
+   return gulp
+      .src('styles/*.less')
+      .pipe(debug({ title: 'Less files:' }))
+      .pipe(gulpless({
+         javascriptEnabled: true,
+         plugins: [new NpmImportPlugin({ prefix: '~' })]
+      }))
+      .pipe(postcss(plugins))
+      .pipe(gulp.dest('dist'))
 });
